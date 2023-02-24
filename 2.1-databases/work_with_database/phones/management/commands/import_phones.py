198cb7ca7173c490017803be6c38c5a3fc9a4e3a
Denis Merkurev <merckuriev.denis@yandex.ru>
2023-02-24 21:01:56 +0300

    First commit for phone_project

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -14,4 +14,16 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            name_split_list = phone['name'].split()
+            name_for_slug = ''
+            for element in name_split_list:
+                name_for_slug += f'{element}-'
+            phone_in_db = Phone(
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exist=phone['lte_exists'],
+                slug=name_for_slug.rstrip('-').lower(),
+            )
+            phone_in_db.save()
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,50 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
+def phones_for_context(phones_object):
+    """Функция для распаковки объектов из БД для последующей передачи в context"""
+    sorted_phones = []
+    for phone in phones_object:
+        inner_dict = {}
+        inner_dict['name'] = phone.name
+        inner_dict['price'] = phone.price
+        inner_dict['image'] = phone.image
+        inner_dict['slug'] = phone.slug
+        inner_dict['release_date'] = phone.release_date
+        inner_dict['lte_exists'] = phone.lte_exist
+        sorted_phones.append(inner_dict)
+    return sorted_phones
+
+
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    parameter = request.GET.get('sort')
+    if parameter == 'name':
+        phones = Phone.objects.order_by('name')
+        phones_list = phones_for_context(phones)
+    elif parameter == 'min_price':
+        phones = Phone.objects.order_by('price')
+        phones_list = phones_for_context(phones)
+    elif parameter == 'max_price':
+        phones = Phone.objects.order_by('price').reverse()
+        phones_list = phones_for_context(phones)
+    else:
+        phones = Phone.objects.all()
+        phones_list = phones_for_context(phones)
+    context = {'phones': phones_list}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phones = Phone.objects.filter(slug=slug)
+    phones_list = phones_for_context(phones)
+    print(slug)
+    context = {'phone': phones_list[0]}
     return render(request, template, context)
